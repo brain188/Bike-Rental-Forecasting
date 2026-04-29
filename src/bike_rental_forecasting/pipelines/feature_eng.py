f48022a9e41fbbdc37f7132f72701abb212da4ec
Tendong Brain Nkengafac <tendongnkengafac@gmail.com>
2026-04-29 19:36:08 +0100

    feat: inference pipeline and app_ui impl

--- src/bike_rental_forecasting/pipelines/feature_eng.py
+++ src/bike_rental_forecasting/pipelines/feature_eng.py
@@ -1,13 +1,13 @@
 from kedro.pipeline import Pipeline, node
 
-from .nodes import get_features, rename_columns
+from .nodes import get_features, rename_columns, load_data
 
 
 def create_feature_eng_pipeline() -> Pipeline:
     return Pipeline([
         node(
             func = rename_columns,
-            inputs = ["train_data", "params:feature_engineering.rename_columns"],
+            inputs = ["input_data", "params:feature_engineering.rename_columns"],
             outputs = "renamed_data",
         ),
         node(
@@ -17,3 +17,37 @@ def create_feature_eng_pipeline() -> Pipeline:
         )
     ])
 
+
+def load_training_data() -> Pipeline:
+    """Load training data pipeline"""
+    return Pipeline(
+        [
+            node(
+                func = load_data,
+                inputs = "train_data",
+                outputs = ["input_data", "last_timestamps"],
+            )
+        ]
+    )
+
+def load_inference_data() -> Pipeline:
+    """Load inference batch pipeline with timestamp extraction."""
+    return Pipeline(
+        [
+            node(
+                func = load_data,
+                inputs = "inference_batch",
+                outputs = ["input_data", "last_timestamps"],
+            )
+        ]
+    )
+
+
+def feat_eng_pipeline_training(**kwargs) -> Pipeline:
+    """Feature engineering pipeline for training."""
+    return load_training_data() + create_feature_eng_pipeline()
+
+
+def feat_eng_pipeline_inference(**kwargs) -> Pipeline:
+    """Feature engineering pipeline for inference."""
+    return load_inference_data() + create_feature_eng_pipeline()
